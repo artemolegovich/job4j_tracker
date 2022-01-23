dedced1c035cc887d26efabbaf6447070be5ff8e
artemolegovich <a.o.lepeshkin@gmail.com>
2022-01-23 20:18:01 +0300

    Отредактированы файлы Tracker.java и TrackerTest.java.

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,27 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String name) {
+        Item[] rsl =  new Item[items.length];
+        int size = 0;
+        for (Item element : items) {
+            if (element != null && name.equals(element.getName())) {
+                rsl[size++] = element;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[items.length];
+        int size = 0;
+        for (Item element : items) {
+            if (element != null) {
+                result[size++] = element;
+            }
+        }
+        return Arrays.copyOf(result, size);
     }
 }
