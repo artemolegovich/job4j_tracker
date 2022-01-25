0f4350fe1a9879fff6cf3b507ed85954eb677f1f
artemolegovich <a.o.lepeshkin@gmail.com>
2022-01-25 17:43:48 +0300

    Отредактирован файл ShopDrop.java.

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -2,18 +2,10 @@ package ru.job4j.pojo;
 
 public class ShopDrop {
     public static Product[] delete(Product[] products, int index) {
-        products[index] = null;
-        for (int i = index; i < products.length; i++) {
-            if (products[i] == null) {
-                for (int j = i + 1; j < products.length; j++) {
-                    if (products[j] != null) {
-                        products[i] = products[j];
-                        products[j] = null;
-                        break;
-                    }
-                }
-            }
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
         }
+        products[products.length - 1] = null;
         return products;
     }
 }
