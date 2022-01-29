c7627b7c18774a9134e14fac4519b3eee8708920
artemolegovich <a.o.lepeshkin@gmail.com>
2022-01-29 10:38:59 +0300

    Добавлен тест для метода deleteItem класса StartUI.

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -29,4 +29,15 @@ public class StartUITest {
         Item edited = tracker.findById(item.getId());
         assertThat(edited.getName(), is("edited item"));
     }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {"1"};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertNull(deleted);
+    }
 }
