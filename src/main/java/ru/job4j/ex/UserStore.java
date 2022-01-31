205e138221f8b8d6336c9b1d9a875938c14b07f9
artemolegovich <a.o.lepeshkin@gmail.com>
2022-01-31 13:25:25 +0300

    Добавлены классы в рамках урока "Иерархия исключений".

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,44 @@
+package ru.job4j.ex;
+
+public class UserStore {
+    public static User findUser(User[] users, String login)
+            throws UserNotFoundException {
+        User rsl = null;
+        for (User user : users) {
+            if (login.equals(user.getUsername())) {
+                rsl = user;
+                break;
+            }
+        }
+        if (rsl == null) {
+            throw new UserNotFoundException("User not found");
+        }
+        return rsl;
+    }
+
+    public static boolean validate(User user)
+            throws UserInvalidException {
+        boolean rsl = user.isValid()
+                && user.getUsername().length() >= 3;
+        if (!rsl) {
+            throw new UserInvalidException("User is not valid");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException i) {
+            i.printStackTrace();
+        } catch (UserNotFoundException nf) {
+            nf.printStackTrace();
+        }
+    }
+}
