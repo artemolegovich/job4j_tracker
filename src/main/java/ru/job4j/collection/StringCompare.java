ce6a1fcaa95a9e345ce605f64d2c1ff4236340cc
artemolegovich <a.o.lepeshkin@gmail.com>
2022-02-17 16:47:21 +0300

    Реализация метода String.compareTo().

---
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int rsl = 0;
+        if (left.length() <= right.length()) {
+            for (int i = 0; i < left.length(); i++) {
+                if (left.charAt(i) != right.charAt(i)) {
+                    rsl = Character.compare(left.charAt(i), right.charAt(i));
+                }
+            }
+        } else {
+            for (int i = 0; i < right.length(); i++) {
+                if (left.charAt(i) != right.charAt(i)) {
+                    rsl = Character.compare(left.charAt(i), right.charAt(i));
+                }
+            }
+        }
+        if (rsl == 0) {
+            rsl = Integer.compare(left.length(), right.length());
+        }
+        return rsl;
+    }
+}
