747f264772c6c78827d66041e66873f471289978
artemolegovich <a.o.lepeshkin@gmail.com>
2022-02-22 16:52:11 +0300

    Изменен класс ScopeInside.

--- src/main/java/ru/job4j/lambda/ScopeInside.java
+++ src/main/java/ru/job4j/lambda/ScopeInside.java
@@ -1,12 +1,22 @@
 package ru.job4j.lambda;
 
+import java.util.function.Function;
+
 public class ScopeInside {
     public static void main(String[] args) {
         int[] number = {1, 2, 3};
-        int total = 0;
-        for (int el : number) {
-            total += el;
-        }
-        System.out.println(total);
+        int rsl = sumEl(number,
+                n -> {
+                    int sum = 0;
+                    for (int el : n) {
+                        sum += el;
+                    }
+                    return sum;
+                });
+        System.out.println(rsl);
+    }
+
+    private static Integer sumEl(int[] array, Function<int[], Integer> calc) {
+        return calc.apply(array);
     }
 }
